ccdf6ef84887d594add6342ae562877d386a1ad9
mohammmod <mohammad.sawas2@outlook.com>
2018-03-25 17:45:40 +0200

    starting with the game

---
+++ src/main/java/at/refugeescode/pset3spring/Pset3springApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.pset3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pset3springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pset3springApplication.class, args);
+	}
+}
